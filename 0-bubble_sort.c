686d93b49882020ef805f94b11ed5b1bd811a485
Cristian Encalada Camargo <cris.encalada.camargo@gmail.com>
2023-05-18 09:36:41 -0300

    Task 0. Printing array after swpping two elements

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -20,12 +20,13 @@ void bubble_sort(int *array, size_t size)
 		for (i = 0; i < size - step - 1; i++)
 		{
 			/* compare two adjacent elements (ascending order) */
-			if (array[i] < array[i + 1])
+			if (array[i] > array[i + 1])
 			{
 				/* swap if elements are not in the intended order */
 				aux = array[i];
 				array[i] = array[i + 1];
 				array[i + 1] = aux;
+				print_array(array, size);
 			}
 		}
 	}
