8341146ef4f35bfa5ec53c96305bed839703deb7
Hiojam <hiojam2016@gmail.com>
2023-05-18 11:32:23 -0500

    Task 3: changed print_array size.

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -20,10 +20,11 @@ void swap(int *a, int *b)
  * @array: The array to be partitioned.
  * @low: The starting index of the partition range.
  * @high: The ending index of the partition range.
+ * @size: The size of the array.
  * 
  * Return: The partition index.
  */
-int lomuto_partition(int *array, int low, int high)
+int lomuto_partition(int *array, int low, int high, size_t size)
 {
 	int pivot = array[high], i = low, j;
 
@@ -37,7 +38,7 @@ int lomuto_partition(int *array, int low, int high)
 	}
 
 	swap(&array[i], &array[high]);
-	print_array(array, high - low + 1);
+	print_array(array, size);
 
 	return i;
 }
@@ -48,17 +49,18 @@ int lomuto_partition(int *array, int low, int high)
  * @array: The array to be sorted.
  * @low: The starting index of the range to be sorted.
  * @high: The ending index of the range to be sorted.
+ * @size: The size of the array.
  */
-void quicksort(int *array, int low, int high)
+void quicksort(int *array, int low, int high, size_t size)
 {
 	int partition_i;
 
 	if (low < high)
 	{
-		partition_i = lomuto_partition(array, low, high);
+		partition_i = lomuto_partition(array, low, high, size);
 
-		quicksort(array, low, partition_i - 1);
-		quicksort(array, partition_i + 1, high);
+		quicksort(array, low, partition_i - 1, size);
+		quicksort(array, partition_i + 1, high, size);
 	}
 }
 
@@ -71,5 +73,5 @@ void quicksort(int *array, int low, int high)
  */
 void quick_sort(int *array, size_t size)
 {
-	quicksort(array, 0, size - 1);
+	quicksort(array, 0, size - 1, size);
 }
