e4666c55674c56982ddde4285aee00b4e51bffaa
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-05-25 13:20:01 -0300

    [6]Fix compilation error. Find max value dinamically

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,7 +1,5 @@
 #include "sort.h"
 
-#define MAX_VALUE 100  /* Adjust the maximum value as needed */
-
 /**
  * counting_sort - Sorts an array of integers
  *			in ascending order using the Counting sort algorithm
@@ -13,36 +11,44 @@
  */
 void counting_sort(int *array, size_t size)
 {
-	char output[size];
-	int *count;
-	int i;
+	int *output, *count;	/* auxiliar arrays */
+	int i, max;
 
-	count = (int *)malloc((MAX_VALUE + 1) * sizeof(int));
+	max = array[0];
+	for (size_t i = 1; i < size; i++)	/* Find the max value in the input array */
+	{
+		if (array[i] > max)
+			max = array[i];
+	}
+	output = malloc(size * sizeof(int));
+	if (output == NULL)
+		return;
+	count = malloc((max + 1) * sizeof(int));
 	if (count == NULL)
 	{
-		fprintf(stderr, "Failed to allocate memory\n");
 		return;
+		free(output);
 	}
-
-	for (i = 0; i <= MAX_VALUE; ++i)
+	/* Initialize all the elements of the count array to 0 */
+	for (i = 0; i <= max; i++)
 		count[i] = 0;
-
-	for (i = 0; i < (int)size; ++i)
-		++count[array[i]];
-
-	for (i = 1; i <= MAX_VALUE; ++i)
+	/* Increment the count for each encountered element */
+	for (i = 0; i < (int)size; i++)
+		count[array[i]]++;
+	/* Update the count array by adding the count of the previous element */
+	for (i = 1; i <= max; i++)
 		count[i] += count[i - 1];
-
-	for (i = size - 1; i >= 0; --i)
+	/* Place each element in its correct position in the output array */
+	for (i = size - 1; i >= 0; i--)
 	{
 		output[count[array[i]] - 1] = array[i];
-		--count[array[i]];
+		count[array[i]]--;
 	}
-
-	for (i = 0; i < (int)size; ++i)
+	/* Copy the sorted elements from the output array back to the input array */
+	for (i = 0; i < (int)size; i++)
 		array[i] = output[i];
 
-
-	/* Print the count array */
-	print_array(count, MAX_VALUE + 1);
+	print_array(count, max + 1);
+	free(output);
+	free(count);
 }
